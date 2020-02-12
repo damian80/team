caeca6b1a5846d9c1303bc7d323261aeb2df9afe
Damian Ciasnocha <daneek80@gmail.com>
2020-02-12 13:46:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const h2Html = document.querySelector('.member h2');
 
 // tworzymy tablice z zdj kol,szarym,imionami i profesja
 
-const colorImages = ['img/s1.png', 'img/s2.png', 'img/s3.png'];
-const greyImages = ['img/s1a.png', 'img/s2a.png', 'img/s3a.png'];
+const colorImages = ['s1.png', 's2.png', 's3.png'];
+const greyImages = ['s1a.png', 's2a.png', 's3a.png'];
 const names = ['Anna Baugart', 'John Smith', 'Jack Black'];
 const professions = ['JS Programmer', 'UX and UI Developer', 'Front-End Developer'];
 
